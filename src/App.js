d312ff4f84412b0619f20b09986243b657a975dd
Ben Hunter <learntocode@benhunter.me>
2021-10-26 16:35:54 -0500

    Tests with Cypress and Jest.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+
+const App = () => {
+    const [isAddRecipeFormDisplayed, setIsAddRecipeFormDisplayed] = useState(false);
+    const [recipeList, setRecipeList] = useState([]);
+    const [newRecipe, setNewRecipe] = useState({
+        name: '',
+        instructions: '',
+    })
+
+    const addNewRecipeForm = (
+        <form id="recipe-form">
+            <label htmlFor="newRecipeName">Recipe name: </label>
+            <input type="text"
+                   id="newRecipeName"
+                   onChange={(event) => (updateNewRecipe(event, 'name'))}/>
+            <label htmlFor="newRecipeInstructions">Recipe Instructions:</label>
+            <textarea
+                id="newRecipeInstructions"
+                placeholder="write recipe instructions here..."
+                onChange={(event) => (updateNewRecipe(event, 'instructions'))}
+            />
+            <input type="submit"
+                   onClick={(event) => submitRecipe(event)}/>
+        </form>
+    );
+
+    const updateNewRecipe = (event, key) => {
+        const recipe = {...newRecipe};
+        recipe[key] = event.target.value;
+        setNewRecipe(recipe);
+    };
+
+    const submitRecipe = (event) => {
+        event.preventDefault();
+
+        // if (newRecipe.name.length > 0 && newRecipe.instructions.length > 0) { }
+        setRecipeList([...recipeList, newRecipe]);
+        setIsAddRecipeFormDisplayed(false);
+    };
+
+    const renderRecipeList = () => recipeList.map((r) =>
+        // <li>Name: {r.name}<br/>
+        //     Instructions: {r.instructions}</li>
+        <li>{r.name}</li>
+    );
+
+    return (
+        <div className="App">
+            <h1 className="App-header">My Recipes</h1>
+            {
+                isAddRecipeFormDisplayed
+                    ? addNewRecipeForm
+                    : <button id="add-recipe"
+                              onClick={() => setIsAddRecipeFormDisplayed(true)}> Add Recipe</button>
+            }
+            {
+                recipeList.length > 0
+                    ? <ul>
+                        {renderRecipeList()}
+                    </ul>
+                    : <p>There are no recipes to list.</p>
+            }
+        </div>
+    )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,74 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+test('Add recipe button toggles visibility of the new recipe form.', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+
+  // queryBy... methods return null if element is not found
+  const recipeForm = screen.queryByText("Instructions:");
+
+  // getBy... methods "throw" an error if element is not found
+  // const recipeForm = screen.getByText("Instructions:");
+
+  expect(recipeForm).toBeNull();
+  userEvent.click(screen.getByText("Add Recipe"));
+
+  expect(screen.getByLabelText("Recipe Instructions:")).toBeInTheDocument();
+
+  expect(screen.queryByText("DOES NOT EXIST")).toBeNull();
+
 });
+
+test('typing in the recipe name makes the recipe name appear in the input', async () => {
+  render(<App />);
+
+  const recipeName = 'No pockets';
+  userEvent.click(screen.getByText("Add Recipe"));
+  await userEvent.type(screen.getByLabelText('Recipe name:'), recipeName);
+
+  expect(screen.getByLabelText('Recipe name:').value).toEqual(recipeName);
+});
+
+const setup = () => {
+  const app = render(<App />);
+
+  userEvent.click(app.getByText('Add Recipe'));
+
+  // Add the submit button to your setup method:
+  const submitButton = app.getByRole('button')
+  const instructionsInput = app.getByLabelText('Recipe Instructions:')
+  const nameInput = app.getByLabelText('Recipe name:')
+
+  return {
+    instructionsInput,
+    nameInput,
+    submitButton
+  }
+}
+
+test('typing in the recipe instructions makes the instructions appear in the form', async () => {
+  const {instructionsInput} = setup();
+
+  const recipeInstructions = "kinda hard to write instructions without knowing what I'm cooking"
+
+  await userEvent.type(instructionsInput, recipeInstructions)
+  expect(instructionsInput.value).toEqual(recipeInstructions);
+});
+
+
+test('recipe name from state appears in an unordered list', async () => {
+  const {instructionsInput, nameInput, submitButton} = setup();
+  const recipeName = "Lean Pockets"
+  const recipeInstructions = "place in toaster oven on 350 for 45 minutes"
+
+  await userEvent.type(instructionsInput, recipeInstructions)
+  await userEvent.type(nameInput, recipeName)
+  userEvent.click(submitButton);
+
+  expect(screen.getByRole('listitem')).toBeInTheDocument();
+  expect(screen.getByText(recipeName)).toBeInTheDocument();
+})
